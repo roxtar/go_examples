1a1ca220a799d7beab0d9553104a33b6a95c4d22
Rohit Kumar <roxtar@gmail.com>
2011-05-28 23:36:47 -0700

    Closed all channels

--- src/pmapreduce.go
+++ src/pmapreduce.go
@@ -2,7 +2,6 @@ package main
 import (
 	"fmt"
 	"runtime"
-	"sync"
 )
 const n = 3000000
 const nprocs = 4
@@ -10,37 +9,31 @@ const nprocs = 4
 func do_map(a []int64, map_udf func(* int64)) {
 
 	block_size := n/nprocs;
-	//ch := make(chan bool, nprocs)
-
-	var wg sync.WaitGroup;
+	ch := make(chan bool, nprocs)	
 
 	for j:=0; j < nprocs; j++ {
-		wg.Add(1)
-		go map_block(a, j*block_size, (j+1)*block_size, &wg, map_udf)
+		go map_block(a, j*block_size, (j+1)*block_size, ch, map_udf)
 	}
 
 	// We simulate a "finish" by draining the channel
 
-	wg.Wait()
-	// for j:=0; j < nprocs; j++ {
-	// 	<- ch
-	// }
+	for j:=0; j < nprocs; j++ {
+	 	<- ch
+	}
+	close(ch)
 }
 
 func map_block(
 	a [] int64,
 	start int,
 	end int,
-	wg *sync.WaitGroup,
-	//ch chan bool,
+	ch chan bool,
 	map_udf func(* int64)) {
 
 	for i:=start; i < end; i++ {
 		map_udf(&a[i])
 	}
-	//ch <- true
-	(*wg).Done()
-	
+	ch <- true
 }
 
 func do_reduce(a []int64) int64{
@@ -54,6 +47,7 @@ func do_reduce(a []int64) int64{
 	for j:=0; j < nprocs; j++ {
 		total += <- ch
 	}
+	close(ch)
 
 	return total
 }
