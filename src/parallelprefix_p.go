3da2b70c09a7bbde8177f1d069dc1d64cc3e5ae0
Raghu Prabhakar <raghu@cs.ucla.edu>
2011-06-02 14:01:19 -0700

    rolled back to prev. parallel prefix

--- src/parallelprefix_p.go
+++ src/parallelprefix_p.go
@@ -6,8 +6,8 @@ import (
 	"flag"
 )
 
-const N = 30000000
-var NPROCS = 4
+const N int64 = 30000000
+var NPROCS int64 = 0
 var a [N]int64
 
 func doInit() {
@@ -21,48 +21,48 @@ func doParallelPrefixMain() {
 	var finish chan bool
 	
 	//Initialize all channels
+	ch = make([]chan int64, NPROCS)
 	finish = make(chan bool, NPROCS)
-	ch = make([] chan int64, NPROCS)
-	for i:= 0; i<NPROCS; i++ {
+	for i:= int64(0); i<NPROCS; i++ {
 		ch[i] = make(chan int64, NPROCS)
 	}
 	
 	//Launch all threads
-	for i:=0; i<NPROCS; i++ {
-		go threadParallelPrefix(i, ch[:], finish)
+	for i:=int64(0); i<NPROCS; i++ {
+		go threadParallelPrefix(i, ch, finish)
 	}
 	
-	//Equivalent to a finish
-	for i:=0; i<NPROCS; i++ {
+	//Equivalent to a reduce
+	for i:=int64(0); i<NPROCS; i++ {
 		<- finish
 	}
-	fmt.Printf("%v\n", a[N-1])
 	
 }
 
-func threadParallelPrefix(pid int, ch []chan int64, finish chan bool) {
+func threadParallelPrefix(pid int64, ch []chan int64, finish chan bool) {
 	//First do a local parallel prefix computation
-	var startIndex int64 = int64(pid) * N/int64(NPROCS)
-	var endIndex int64 = startIndex + (N/int64(NPROCS))
-	var toBeAdded int64 = 0
-
-	for i:=startIndex+1; i<endIndex; i++ {		
-		a[i] = a[i] + a[i-1]
+	var startIndex int64 = pid*N/NPROCS
+	var endIndex int64 = startIndex + (N/NPROCS)
+	var localSum int64 = a[startIndex]
+	for i:=startIndex+1; i<endIndex; i++ {
+		localSum += a[i]
+		a[i] = localSum
 	}
 	
-	for i:= pid+1; i<NPROCS; i++ {		
-		ch[i] <- a[i-1]
+	for i:= pid+1; i<NPROCS; i++ {
+		
+		ch[i] <- localSum
 	}
 	
-	for i:=0; i < pid; i++ {
-		 toBeAdded += <- ch[pid]
-	}
-			
-	for j:=startIndex; j<endIndex; j++ {
-		a[j] += toBeAdded
+	for i := int64(0); i<pid; i++ {
+		toBeAdded := <- ch[pid]
+		for j:=startIndex; j<endIndex; j++ {
+			a[j] += toBeAdded
+		}
 	}
 	 
-	finish <- true	
+	finish <- true
+	
 }  
 func printArray() {
 for i:=0; i<len(a); i++ {
@@ -70,10 +70,16 @@ for i:=0; i<len(a); i++ {
 }
 }
 
+func init() {
+	flag.Int64Var(&NPROCS, "n", 1, "Number of threads");
+	flag.Parse();
+}
+
 func main() {
-	flag.IntVar(&NPROCS, "n", 1, "Number of threads")
-	flag.Parse()
 	runtime.GOMAXPROCS(int(NPROCS))
 	doInit()
 	doParallelPrefixMain()
+	fmt.Println(a[len(a)-1])
+//	printArray();
+// 	fmt.Println("Hello world!!")
 }
